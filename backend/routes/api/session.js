eaa5fb33d4b14ba2454458d40d78ab784aa6625f
Katharine <katharine.arburn@gmail.com>
2024-01-30 17:34:05 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,7 +8,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-
+// Log in
 router.post(
     '/',
     async (req, res, next) => {
@@ -45,5 +45,13 @@ router.post(
     }
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
 
 module.exports = router;
